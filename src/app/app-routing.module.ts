f47a58daf98184c47b9fe55a9523f914d72147b7
YourCreator <65864759+Your-Creator@users.noreply.github.com>
2021-01-22 20:27:24 +0530

    Home Solved

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { ServicesComponent } from './components/services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:"",
+  component:HomeComponent,
+  pathMatch:"full",
+}
+,
+{
+  path:"Services",
+  component:ServicesComponent,
+  pathMatch:"full",
+}
+,
+{
+  path:"About",
+  component:AboutComponent,
+  pathMatch:"full",
+}
+,
+{
+  path:"Contact",
+  component:ContactComponent,
+  pathMatch:"full",
+}
+,
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
